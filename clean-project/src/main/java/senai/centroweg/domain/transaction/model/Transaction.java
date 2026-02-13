c2b69be116995edb6f093a3b6633451fcf1d84d3
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-13 18:50:31 -0300

    feat: add Transaction Entity

--- clean-project/src/main/java/senai/centroweg/domain/transaction/model/Transaction.java
+++ clean-project/src/main/java/senai/centroweg/domain/transaction/model/Transaction.java
@@ -3,6 +3,7 @@ package senai.centroweg.domain.transaction.model;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
@@ -12,5 +13,27 @@ public class Transaction {
     private final UUID senderAccountId;
     private final UUID receiverAccountId;
     private final BigDecimal amount;
+    private final TransactionType type;
+    private final Instant createdAt;
+    private Instant confirmationAt;
 
+    public Transaction(UUID id, UUID senderAccountId, UUID receiverAccountId, BigDecimal amount, TransactionType type, Instant createdAt, Instant confirmationAt) {
+        this.id = id;
+        this.senderAccountId = senderAccountId;
+        this.receiverAccountId = receiverAccountId;
+        this.amount = amount;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.confirmationAt = confirmationAt;
+    }
+
+    public Transaction(UUID senderAccountId, UUID receiverAccountId, BigDecimal amount, TransactionType type, Instant createdAt, Instant confirmationAt) {
+        this.id = UUID.randomUUID();
+        this.senderAccountId = senderAccountId;
+        this.receiverAccountId = receiverAccountId;
+        this.amount = amount;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.confirmationAt = confirmationAt;
+    }
 }
