f5d02bc3de8407bcfe813f422645b991b210b117
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-13 18:47:08 -0300

    feat: add Transaction Type ENUM

--- clean-project/src/main/java/senai/centroweg/domain/transaction/model/Transaction.java
+++ clean-project/src/main/java/senai/centroweg/domain/transaction/model/Transaction.java
@@ -1,4 +1,16 @@
 package senai.centroweg.domain.transaction.model;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
 public class Transaction {
+
+    private final UUID id;
+    private final UUID senderAccountId;
+    private final UUID receiverAccountId;
+    private final BigDecimal amount;
+
 }
