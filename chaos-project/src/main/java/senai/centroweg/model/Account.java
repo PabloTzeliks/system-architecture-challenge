eecb35b83c5fedc469d18cffa4222da729fdae5e
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-13 15:52:24 -0300

    feat: add Account Model Class for Chaos

--- chaos-project/src/main/java/senai/centroweg/model/Account.java
+++ chaos-project/src/main/java/senai/centroweg/model/Account.java
@@ -1,4 +1,36 @@
 package senai.centroweg.model;
 
 public class Account {
+
+    public int id;
+    public float balance;
+
+    public Account(int id, float balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", balance=" + balance +
+                '}';
+    }
 }
