bf9ca9f27f16914e62f75f7e76381fe138b13650
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-13 18:39:50 -0300

    chore: add Lombok dependency

--- clean-project/src/main/java/senai/centroweg/domain/user/model/User.java
+++ clean-project/src/main/java/senai/centroweg/domain/user/model/User.java
@@ -1,4 +1,19 @@
 package senai.centroweg.domain.user.model;
 
+import java.util.UUID;
+
 public class User {
+
+    private final UUID id;
+    private String username;
+
+    public User(UUID id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public User(String username) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+    }
 }
