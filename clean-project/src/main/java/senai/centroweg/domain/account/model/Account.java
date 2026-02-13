fcbe9a9de9b478fbee47cf3693cc579a38030e67
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-13 18:52:30 -0300

    feat: add Account Entity

--- clean-project/src/main/java/senai/centroweg/domain/account/model/Account.java
+++ clean-project/src/main/java/senai/centroweg/domain/account/model/Account.java
@@ -1,4 +1,27 @@
 package senai.centroweg.domain.account.model;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
 public class Account {
+
+    private final UUID id;
+    private BigDecimal balance;
+    private final Instant createdAt;
+
+    public Account(UUID id, BigDecimal balance, Instant createdAt) {
+        this.id = id;
+        this.balance = balance;
+        this.createdAt = createdAt;
+    }
+
+    public Account(BigDecimal balance, Instant createdAt) {
+        this.id = UUID.randomUUID();
+        this.balance = balance;
+        this.createdAt = createdAt;
+    }
 }
