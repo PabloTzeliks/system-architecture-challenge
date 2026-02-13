d9e8ac38b3cae8328677b9a4f7a93b91687df444
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-13 20:25:05 -0300

    feat: implement Domain Methods for an Account

--- clean-project/src/main/java/senai/centroweg/domain/account/model/Account.java
+++ clean-project/src/main/java/senai/centroweg/domain/account/model/Account.java
@@ -1,6 +1,7 @@
 package senai.centroweg.domain.account.model;
 
 import lombok.Data;
+import senai.centroweg.domain.account.exception.DomainException;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -23,5 +24,25 @@ public class Account {
         this.id = UUID.randomUUID();
         this.balance = balance;
         this.createdAt = createdAt;
+    }
+
+    public void credit(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new DomainException("Valor de crédito deve ser positivo");
+        }
+
+        this.balance = this.balance.add(amount);
+    }
+
+    public void debit(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new DomainException("Valor de débito deve ser positivo");
+        }
+
+        if (this.balance.compareTo(amount) < 0) {
+            throw new DomainException("Saldo insuficiente");
+        }
+
+        this.balance = this.balance.subtract(amount);
     }
 }
