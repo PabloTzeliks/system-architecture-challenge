32ea5bb436c5f246037a11218c886a9586c1d988
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-13 15:56:14 -0300

    feat: implement Transaction Model Class for Chaos

--- chaos-project/src/main/java/senai/centroweg/model/Transaction.java
+++ chaos-project/src/main/java/senai/centroweg/model/Transaction.java
@@ -1,4 +1,58 @@
 package senai.centroweg.model;
 
 public class Transaction {
+
+    public int id;
+    public int senderAccountId;
+    public int receiverAccountId;
+    public float amount;
+    public String transactionType;
+
+    public Transaction(int id, int senderAccountId, int receiverAccountId, float amount, String transactionType) {
+        this.id = id;
+        this.senderAccountId = senderAccountId;
+        this.receiverAccountId = receiverAccountId;
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSenderAccountId() {
+        return senderAccountId;
+    }
+
+    public void setSenderAccountId(int senderAccountId) {
+        this.senderAccountId = senderAccountId;
+    }
+
+    public int getReceiverAccountId() {
+        return receiverAccountId;
+    }
+
+    public void setReceiverAccountId(int receiverAccountId) {
+        this.receiverAccountId = receiverAccountId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
 }
