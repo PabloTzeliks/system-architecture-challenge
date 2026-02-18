8a8e6eda6a1de15f8fdee0e36f4e622e6dbe2bb0
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-18 20:21:10 -0300

    feat: implement DebitCardStrategy

--- clean-project/src/main/java/senai/centroweg/domain/transaction/strategy/impl/CreditCardStrategy.java
+++ clean-project/src/main/java/senai/centroweg/domain/transaction/strategy/impl/CreditCardStrategy.java
@@ -1,4 +1,16 @@
 package senai.centroweg.domain.transaction.strategy.impl;
 
-public class CreditCardStrategy {
+import senai.centroweg.domain.transaction.model.TransactionType;
+import senai.centroweg.domain.transaction.strategy.FeeCalculationStrategy;
+
+import java.math.BigDecimal;
+
+public class CreditCardStrategy implements FeeCalculationStrategy {
+
+    @Override
+    public BigDecimal calculate(BigDecimal amount, TransactionType type) {
+
+        BigDecimal percentage = BigDecimal.valueOf(TransactionType..getTax());
+        return amount.multiply(percentage);
+    }
 }
