469d243130788f0dc4b767055b577e9416f6d7cb
bruno_l_medeiros <bruno_l_medeiros@estudante.sesisenai.org.br>
2026-02-18 15:40:42 -0300

    feat: define exception for missing accounts

--- clean-project/src/main/java/senai/centroweg/application/transaction/usecase/TransferFundsUseCase.java
+++ clean-project/src/main/java/senai/centroweg/application/transaction/usecase/TransferFundsUseCase.java
@@ -1,4 +1,27 @@
 package senai.centroweg.application.transaction.usecase;
 
+import senai.centroweg.domain.account.exception.AccountNotFoundException;
+import senai.centroweg.domain.account.model.Account;
+import senai.centroweg.domain.account.ports.AccountRepositoryPort;
+import senai.centroweg.domain.transaction.model.Transaction;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class TransferFundsUseCase {
+    AccountRepositoryPort accountRepository;
+
+    public TransferFundsUseCase(AccountRepositoryPort accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Transaction execute(UUID senderId, UUID receiverId, BigDecimal amount) {
+
+        Account senderAccount = accountRepository.findById(senderId)
+                .orElseThrow(() -> new AccountNotFoundException("Conta com ID " + senderId + " não encontrada"));
+        Account receiverAccount = accountRepository.findById(receiverId)
+                .orElseThrow(() -> new AccountNotFoundException("Conta com ID " + senderId + " não encontrada"));
+
+
+    }
 }
---
+++ clean-project/src/main/java/senai/centroweg/domain/account/exception/AccountNotFoundException.java
@@ -0,0 +1,7 @@
+package senai.centroweg.domain.account.exception;
+
+public class AccountNotFoundException extends DomainException {
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
