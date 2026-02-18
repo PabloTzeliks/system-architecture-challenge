782c478f4a1a6ecc0e157959b7a0698ad59e84d9
bruno_l_medeiros <bruno_l_medeiros@estudante.sesisenai.org.br>
2026-02-18 15:44:20 -0300

    refactor: use Optional in findById to enforce domain validation

--- clean-project/src/main/java/senai/centroweg/domain/account/ports/AccountRepositoryPort.java
+++ clean-project/src/main/java/senai/centroweg/domain/account/ports/AccountRepositoryPort.java
@@ -2,11 +2,12 @@ package senai.centroweg.domain.account.ports;
 
 import senai.centroweg.domain.account.model.Account;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AccountRepositoryPort {
 
     Account save(Account account);
 
-    Account findById(UUID id);
+    Optional<Account> findById(UUID id);
 }
