051e8e36476239fd5024a6da5f1d485f3b42643d
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-18 20:19:46 -0300

    feat: implement TedStrategy

--- clean-project/src/main/java/senai/centroweg/domain/transaction/strategy/impl/TedStrategy.java
+++ clean-project/src/main/java/senai/centroweg/domain/transaction/strategy/impl/TedStrategy.java
@@ -1,4 +1,16 @@
 package senai.centroweg.domain.transaction.strategy.impl;
 
-public class TedStrategy {
+import senai.centroweg.domain.transaction.model.TransactionType;
+import senai.centroweg.domain.transaction.strategy.FeeCalculationStrategy;
+
+import java.math.BigDecimal;
+
+public class TedStrategy implements FeeCalculationStrategy {
+
+    @Override
+    public BigDecimal calculate(BigDecimal amount, TransactionType type) {
+
+        BigDecimal percentage = BigDecimal.valueOf(TransactionType.TED.getTax());
+        return amount.multiply(percentage);
+    }
 }
