570840da5a5aa2e2f280c8c21003921b8bb96152
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-19 10:29:08 -0300

    refactor: set new approach for creating a Database Connection

--- clean-project/src/main/java/senai/centroweg/infrastructure/persistence/database/QueryExecutor.java
+++ clean-project/src/main/java/senai/centroweg/infrastructure/persistence/database/QueryExecutor.java
@@ -7,16 +7,22 @@ import java.sql.Statement;
 
 public class QueryExecutor {
 
+    private final DataSource dataSource;
+
+    public QueryExecutor(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     public <R> R extract(final String query, final StatementCallback<R> extractor) {
 
-        try (var connection = DataSource.getConnection();
+        try (var connection = dataSource.getConnection();
              var preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
 
             preparedStatement.clearParameters();
 
             return extractor.run(preparedStatement);
         } catch (SQLException ex) {
-            throw new DatabaseException("Erro no banco de dados: " +ex.getMessage());
+            throw new DatabaseException("Erro no banco de dados: " + ex.getMessage(), ex);
         }
     }
 }
--- clean-project/src/main/java/senai/centroweg/infrastructure/persistence/exception/DatabaseException.java
+++ clean-project/src/main/java/senai/centroweg/infrastructure/persistence/exception/DatabaseException.java
@@ -2,8 +2,10 @@ package senai.centroweg.infrastructure.persistence.exception;
 
 import senai.centroweg.infrastructure.common.exception.InfrastructureException;
 
+import java.sql.SQLException;
+
 public class DatabaseException extends InfrastructureException {
-    public DatabaseException(String message) {
+    public DatabaseException(String message, SQLException exception) {
         super(message);
     }
 }
