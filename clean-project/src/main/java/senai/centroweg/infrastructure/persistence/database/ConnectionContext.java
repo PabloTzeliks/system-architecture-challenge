62b12509dfa5507ec4ae4c139f9272e7f6f53c5a
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-19 10:54:04 -0300

    feat: implement Connection Context class

---
+++ clean-project/src/main/java/senai/centroweg/infrastructure/persistence/database/ConnectionContext.java
@@ -0,0 +1,20 @@
+package senai.centroweg.infrastructure.persistence.database;
+
+import java.sql.Connection;
+
+public class ConnectionContext {
+
+    private static ThreadLocal<Connection> CONTEXT = new ThreadLocal<>();
+
+    public static void set(Connection connection) {
+        CONTEXT.set(connection);
+    }
+
+    public static Connection get() {
+        return CONTEXT.get();
+    }
+
+    public static void remove() {
+        CONTEXT.remove();
+    }
+}
