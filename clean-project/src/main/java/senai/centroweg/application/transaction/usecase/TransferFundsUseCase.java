e6ccc7ef0a74a1c5a0d65315a578c2858dcec773
Bruno Luis <bruno_l_medeiros@estudante.sesisenai.org.br>
2026-02-19 14:38:07 -0300

    chore: setup dependency injection for transfer use case

--- clean-project/src/main/java/senai/centroweg/application/transaction/usecase/TransferFundsUseCase.java
+++ clean-project/src/main/java/senai/centroweg/application/transaction/usecase/TransferFundsUseCase.java
@@ -1,8 +1,12 @@
 package senai.centroweg.application.transaction.usecase;
 
+import senai.centroweg.application.transaction.command.TransferCommand;
+import senai.centroweg.application.transaction.manager.TransactionManager;
 import senai.centroweg.domain.account.exception.AccountNotFoundException;
 import senai.centroweg.domain.account.model.Account;
 import senai.centroweg.domain.account.ports.AccountRepositoryPort;
+import senai.centroweg.domain.entry.model.Entry;
+import senai.centroweg.domain.entry.ports.EntryRepositoryPort;
 import senai.centroweg.domain.transaction.model.Transaction;
 import senai.centroweg.domain.transaction.model.TransactionType;
 import senai.centroweg.domain.transaction.ports.TransactionRepositoryPort;
@@ -13,12 +17,21 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public class TransferFundsUseCase {
-    AccountRepositoryPort accountRepository;
-    TransactionRepositoryPort transactionRepository;
 
-    public TransferFundsUseCase(AccountRepositoryPort accountRepository, TransactionRepositoryPort transactionRepository) {
+    private final AccountRepositoryPort accountRepository;
+    private final TransactionRepositoryPort transactionRepository;
+    private final EntryRepositoryPort entryRepository;
+    private final TransactionManager transactionManager;
+
+    public TransferFundsUseCase(AccountRepositoryPort accountRepository,
+                                TransactionRepositoryPort transactionRepository,
+                                EntryRepositoryPort entryRepository,
+                                TransactionManager transactionManager) {
+
         this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
+        this.entryRepository = entryRepository;
+        this.transactionManager = transactionManager;
     }
 
     public Transaction execute(UUID senderId, UUID receiverId, BigDecimal amount, TransactionType type) {
