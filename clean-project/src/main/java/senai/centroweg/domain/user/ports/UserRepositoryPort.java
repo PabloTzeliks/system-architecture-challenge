94e545a93c9361066e55229630dfc57ac76be259
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-19 12:27:39 -0300

    feat: implement UserRepositoryAdapter

--- clean-project/src/main/java/senai/centroweg/domain/user/ports/UserRepositoryPort.java
+++ clean-project/src/main/java/senai/centroweg/domain/user/ports/UserRepositoryPort.java
@@ -2,11 +2,12 @@ package senai.centroweg.domain.user.ports;
 
 import senai.centroweg.domain.user.model.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepositoryPort {
 
     User create(User user);
 
-    User findById(UUID id);
+    Optional<User> findById(UUID id);
 }
