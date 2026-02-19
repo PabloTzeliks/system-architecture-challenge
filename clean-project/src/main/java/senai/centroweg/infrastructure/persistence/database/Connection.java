6a388c1d701507c443a604ea107c506f98ae73ee
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-19 09:43:34 -0300

    feat: add initial Connection Database class

--- clean-project/src/main/java/senai/centroweg/infrastructure/persistence/database/Connection.java
+++ clean-project/src/main/java/senai/centroweg/infrastructure/persistence/database/Connection.java
@@ -1,4 +1,15 @@
 package senai.centroweg.infrastructure.persistence.database;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connection {
+
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
