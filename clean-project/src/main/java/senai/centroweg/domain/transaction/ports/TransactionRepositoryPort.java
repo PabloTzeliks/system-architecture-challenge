ef3f923793a851480c2d5305f265c677ef8813a4
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-18 21:39:07 -0300

    feat: implement Repository Port for Transaction

--- clean-project/src/main/java/senai/centroweg/domain/transaction/ports/TransactionRepositoryPort.java
+++ clean-project/src/main/java/senai/centroweg/domain/transaction/ports/TransactionRepositoryPort.java
@@ -1,4 +1,8 @@
 package senai.centroweg.domain.transaction.ports;
 
+import senai.centroweg.domain.transaction.model.Transaction;
+
 public interface TransactionRepositoryPort {
+
+    Transaction save(Transaction transaction);
 }
