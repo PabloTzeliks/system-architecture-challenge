22ec9f4c44c926f3a11751188f5f7de5ab802eb4
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-20 14:53:17 -0300

    fix: adjust User UseCase

--- clean-project/src/main/java/senai/centroweg/application/user/manager/UserManager.java
+++
@@ -1,6 +0,0 @@
-package senai.centroweg.application.user.manager;
-
-@FunctionalInterface
-public interface UserManager {
-    void execute(Runnable action);
-}
--- clean-project/src/main/java/senai/centroweg/application/user/usecase/AddNewUserUseCase.java
+++ clean-project/src/main/java/senai/centroweg/application/user/usecase/AddNewUserUseCase.java
@@ -1,30 +1,21 @@
 package senai.centroweg.application.user.usecase;
 
 import senai.centroweg.application.user.command.NewUserCommand;
-import senai.centroweg.application.user.manager.UserManager;
 import senai.centroweg.domain.user.model.User;
 import senai.centroweg.domain.user.ports.UserRepositoryPort;
 
 public class AddNewUserUseCase {
 
     private final UserRepositoryPort userRepository;
-    private final UserManager userManager;
 
-    public AddNewUserUseCase(UserRepositoryPort userRepository,
-                             UserManager userManager) {
-
+    public AddNewUserUseCase(UserRepositoryPort userRepository) {
         this.userRepository = userRepository;
-        this.userManager = userManager;
     }
 
     public User execute(NewUserCommand userCommand) {
 
         User user = new User(userCommand.username());
 
-        userManager.execute(() -> {
-            userRepository.save(user);
-        });
-
-        return user;
+        return userRepository.save(user);
     }
 }
