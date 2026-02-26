9ee2c18fa571454fbbb3e7024496cdfc117165cb
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-26 14:33:22 -0300

    chore: add SQL database schema for Integration Tests

---
+++ clean-project/src/test/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id UUID PRIMARY KEY,
+    sender_account_id UUID NOT NULL,
+    receiver_account_id UUID NOT NULL,
+    raw_amount DECIMAL(19,4) NOT NULL,
+    fee_amount DECIMAL(19,4) NOT NULL,
+    total_amount DECIMAL(19,4) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    confirmation_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS entries (
+    id UUID PRIMARY KEY DEFAULT random_uuid(),
+    account_id UUID NOT NULL,
+    transaction_id UUID NOT NULL,
+    amount DECIMAL(19,4) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(id),
+    FOREIGN KEY (transaction_id) REFERENCES transactions(id)
+);
